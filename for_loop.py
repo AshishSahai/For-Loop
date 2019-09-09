ce0073ab46d1afc04794d48764c52fb5c0b5df63
Wade <aksahaiashish87@gmail.com>
2019-09-09 07:32:37 +0530

    Create for_loop.py

---
+++ for_loop.py
@@ -0,0 +1,14 @@
+#making an array of friends
+
+
+friends=["Jim", "Darla", "Elias", "Jon"]
+result=1
+
+
+#selecting each friend from index 0  and printing them
+
+
+for index in range(len(friends)):
+  print(friends[index])
+  
+  
